2f7c2bd0eecea8b9b71eafee368f100bd6a96146
Akash Gautam <76935019+geekblower@users.noreply.github.com>
2023-02-06 22:50:14 +0530

    Update 6_Volume_Of_Cone.c

--- 106 - Lab on C/03_Opeartors_II/6_Volume_Of_Cone.c
+++ 106 - Lab on C/03_Opeartors_II/6_Volume_Of_Cone.c
@@ -7,5 +7,15 @@
 #include <stdio.h>
 
 int main() {
+    int rad, hgt;
+    printf("Enter the radius of the cone : ");
+    scanf("%d", &rad);
+    printf("Enter the height of the cone : ");
+    scanf("%d", &hgt);
+
+    float vol = (3.14 * rad * rad * hgt) / 3;
+
+    printf("Volume of the cone : %.2f", vol);
+
     return 0;
 }
