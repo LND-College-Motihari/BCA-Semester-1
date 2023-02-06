dd4ebd368a3c29c56d69dae319466ea508f6f018
Akash Gautam <76935019+geekblower@users.noreply.github.com>
2023-02-06 22:58:18 +0530

    Update 3_Prime_Numbers_In_Given_Range.c

--- 106 - Lab on C/06_Basic_Loop_Operations/3_Prime_Numbers_In_Given_Range.c
+++ 106 - Lab on C/06_Basic_Loop_Operations/3_Prime_Numbers_In_Given_Range.c
@@ -7,5 +7,30 @@
 #include <stdio.h>
 
 int main() {
+
+    int start, end;
+    printf("Enter the start limit : ");
+    scanf("%d", &start);
+    printf("Enter the end limitm : ");
+    scanf("%d", &end);
+
+    printf("All prime numbers between %d and %d : ", start, end);
+
+    for(int i=start; i<=end; i++) {
+        int temp = i;
+        int flag = 1;
+
+        for(int j=2; j<=temp/2; j++) {
+            if(temp%j == 0) {
+                flag = 0;
+                break;
+            }
+        }
+
+        if(flag) {
+            printf("%d ", temp);
+        }
+    }
+
     return 0;
 }
