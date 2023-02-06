f5bf2607bab394c64ac9c4220547d62d7f964211
Akash Gautam <76935019+geekblower@users.noreply.github.com>
2023-02-06 23:06:26 +0530

    Update 4_Display_Multiplication_Table.c

--- 106 - Lab on C/06_Basic_Loop_Operations/4_Display_Multiplication_Table.c
+++ 106 - Lab on C/06_Basic_Loop_Operations/4_Display_Multiplication_Table.c
@@ -7,5 +7,18 @@
 #include <stdio.h>
 
 int main() {
+
+    for(int i=1; i<=10; i++) {
+        if(i==3 || i==5) {
+            continue;
+        }
+
+        printf("\nTable of %d : ", i);
+
+        for(int j=1; j<=10; j++) {
+            printf("%d ", i*j);
+        }
+    }
+    
     return 0;
 }
