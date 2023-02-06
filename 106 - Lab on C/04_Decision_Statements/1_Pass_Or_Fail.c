bda4d4958c4a8bd2a1f46d4149d5d4f7c7190144
Akash Gautam <76935019+geekblower@users.noreply.github.com>
2023-02-06 22:56:02 +0530

    Update 1_Pass_Or_Fail.c

--- 106 - Lab on C/04_Decision_Statements/1_Pass_Or_Fail.c
+++ 106 - Lab on C/04_Decision_Statements/1_Pass_Or_Fail.c
@@ -7,5 +7,30 @@
 #include <stdio.h>
 
 int main() {
+    int m1, m2, m3, m4, m5, m6, tot;
+    float avg;
+
+    printf("Enter the marks in subject 1 : ");
+    scanf("%d", &m1);
+    printf("Enter the marks in subject 2 : ");
+    scanf("%d", &m2);
+    printf("Enter the marks in subject 3 : ");
+    scanf("%d", &m3);
+    printf("Enter the marks in subject 4 : ");
+    scanf("%d", &m4);
+    printf("Enter the marks in subject 5 : ");
+    scanf("%d", &m5);
+    printf("Enter the marks in subject 6 : ");
+    scanf("%d", &m6);
+
+    tot = m1 + m2 + m3 + m4 + m5 + m6;
+    avg = tot / 6;
+
+    if(avg >= 33.33) {
+        printf("PASS");
+    } else {
+        printf("FAIL");
+    }
+
     return 0;
 }
