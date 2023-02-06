4b5ea9f79cae0710cf067ba430172ea348038d46
Akash Gautam <76935019+geekblower@users.noreply.github.com>
2023-02-06 22:46:15 +0530

    Update 3_Pre_And_Post_Increment.c

--- 106 - Lab on C/03_Opeartors_II/3_Pre_And_Post_Increment.c
+++ 106 - Lab on C/03_Opeartors_II/3_Pre_And_Post_Increment.c
@@ -7,5 +7,14 @@
 #include <stdio.h>
 
 int main() {
+
+    int a = 5;
+
+    printf("a = %d\n", a);
+    printf("++a = %d\n", ++a);
+    printf("a++ = %d\n", a++);
+
+    printf("a = %d && ++a = %d && a++ = %d", a, ++a, a++);
+
     return 0;
 }
