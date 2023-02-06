787fd72eba5dae60563c4ada7cb008107ea92f86
Akash Gautam <76935019+geekblower@users.noreply.github.com>
2023-02-06 22:45:03 +0530

    Update 1_Relational_Operators.c

--- 106 - Lab on C/03_Opeartors_II/1_Relational_Operators.c
+++ 106 - Lab on C/03_Opeartors_II/1_Relational_Operators.c
@@ -7,5 +7,13 @@
 #include <stdio.h>
 
 int main() {
+    int a = 50;
+    int b = 100;
+
+    printf("a <= b : %d\n", a<=b);
+    printf("a >= b : %d\n", a>=b);
+    printf("a == b : %d\n", a==b);
+    printf("a != b : %d\n", a!=b);
+
     return 0;
 }
