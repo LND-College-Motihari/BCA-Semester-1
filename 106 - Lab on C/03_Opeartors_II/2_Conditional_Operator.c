df4578c83c844068ab33b7cd75c46ff5dba0ec3e
Akash Gautam <76935019+geekblower@users.noreply.github.com>
2023-02-06 22:45:35 +0530

    Update 2_Conditional_Operator.c

--- 106 - Lab on C/03_Opeartors_II/2_Conditional_Operator.c
+++ 106 - Lab on C/03_Opeartors_II/2_Conditional_Operator.c
@@ -7,5 +7,15 @@
 #include <stdio.h>
 
 int main() {
+    int num1, num2;
+
+    printf("Enter first number : ");
+    scanf("%d", &num1);
+    
+    printf("Enter second number : ");
+    scanf("%d", &num2);
+
+    (num1==num2) ? printf("Equal!") : printf("Not equal!");
+
     return 0;
 }
