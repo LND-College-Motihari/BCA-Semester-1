47d9e79b3b51e81e2ecb387418a0485fe31a3383
Akash Gautam <76935019+geekblower@users.noreply.github.com>
2023-02-07 12:14:18 +0530

    Update 2_Roots_Of_Quadratic_Equation.c

--- 106 - Lab on C/04_Decision_Statements/2_Roots_Of_Quadratic_Equation.c
+++ 106 - Lab on C/04_Decision_Statements/2_Roots_Of_Quadratic_Equation.c
@@ -5,7 +5,34 @@
 */
 
 #include <stdio.h>
+#include <math.h>
 
 int main() {
+    float a, b, c;
+
+    printf("Enter the co-efficient of x^2 : ");
+    scanf("%f", &a);
+
+    printf("Enter the co-efficient of x : ");
+    scanf("%f", &b);
+
+    printf("Enter the constant term : ");
+    scanf("%f", &c);
+
+    float det = (b*b) - (4*a*c);
+
+    if(det < 0) {
+        printf("Roots doesn't exist!");
+    } else if(det == 0) {
+        float root = b / (2*a);
+        printf("Roots : %.2f & %.2f", root, root);
+    } else {
+        det = sqrt(det);
+        float root1 = (-b+det) / (2*a);
+        float root2 = (-b-det) / (2*a);
+
+        printf("Roots : %.2f & %.2f", root1, root2);
+    }
+
     return 0;
 }
