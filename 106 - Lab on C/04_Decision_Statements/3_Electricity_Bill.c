5dfa946d3aa3d11eee9d96528e39127e3c90fc32
Akash Gautam <76935019+geekblower@users.noreply.github.com>
2023-02-07 12:11:19 +0530

    Update 3_Electricity_Bill.c

--- 106 - Lab on C/04_Decision_Statements/3_Electricity_Bill.c
+++ 106 - Lab on C/04_Decision_Statements/3_Electricity_Bill.c
@@ -7,5 +7,52 @@
 #include <stdio.h>
 
 int main() {
+    int start, end;
+
+    printf("Enter the starting meter reading : ");
+    scanf("%d", &start);
+    
+    printf("Enter the ending meter reading : ");
+    scanf("%d", &end);
+
+    float amount = 0;
+    int unit = end - start;
+
+    if(unit < 1) {
+        printf("Invalid input!");
+    }
+
+    if(unit > 500) {
+        amount = (100*1.50) + (200*2.00) + (200*2.50);
+        unit -= 500;
+
+        amount += unit * 3.25;
+        unit = 0;
+    }
+
+    
+    if(unit > 300) {
+        amount = (100*1.50) + (200*2.00);
+        unit -= 300;
+
+        amount += unit * 2.50;
+        unit = 0;
+    }
+
+    
+    if(unit > 100) {
+        amount = (100*1.50);
+        unit -= 100;
+
+        amount += unit * 2.00;
+        unit = 0;
+    }
+
+    if(unit > 0) {
+        amount = unit * 1.50;
+    }
+
+    printf("\nBill Amount : %.2f", amount);
+
     return 0;
 }
